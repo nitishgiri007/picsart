17c6534d2472fe097fceaf9ddb18eb1f98f3a939
nitishgiri007 <85404432+nitishgiri007@users.noreply.github.com>
2021-06-17 04:51:41 -0700

    Update Title.js

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="title">
       <h1>Pics_Art</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Click on plus icon to add poctures to the gallary.</p>
     </div>
   )
 }
